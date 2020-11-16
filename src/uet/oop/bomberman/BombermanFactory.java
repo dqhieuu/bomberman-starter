603eddd59e71f373b749bf82db2289c18c3de0eb
Hieu <dqhieu01@gmail.com>
2020-11-16 19:09:52 +0700

    Try something

---
+++ src/uet/oop/bomberman/BombermanFactory.java
@@ -0,0 +1,7 @@
+package uet.oop.bomberman;
+
+import com.almasb.fxgl.texture.AnimationChannel;
+
+public class BombermanFactory {
+
+}
--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -1,22 +1,34 @@
 package uet.oop.bomberman;
 
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.settings.GameSettings;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.RadioButton;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import uet.oop.bomberman.entities.Bomber;
-import uet.oop.bomberman.entities.Entity;
+
 import uet.oop.bomberman.entities.Grass;
 import uet.oop.bomberman.entities.Wall;
 import uet.oop.bomberman.graphics.Sprite;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class BombermanGame extends Application {
+public class BombermanGame extends GameApplication {
     
     public static final int WIDTH = 20;
     public static final int HEIGHT = 15;
@@ -26,65 +38,130 @@ public class BombermanGame extends Application {
     private List<Entity> entities = new ArrayList<>();
     private List<Entity> stillObjects = new ArrayList<>();
 
+    private Entity player;
+
 
     public static void main(String[] args) {
-        Application.launch(BombermanGame.class);
+        launch(args);
+    }
+
+//    @Override
+//    public void start(Stage stage) {
+//        // Tao Canvas
+//        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
+//        gc = canvas.getGraphicsContext2D();
+//
+//        // Tao root container
+//        Group root = new Group();
+//        root.getChildren().add(canvas);
+//
+//        // Tao scene
+//        Scene scene = new Scene(root);
+//
+//        // Them scene vao stage
+//        stage.setScene(scene);
+//        stage.show();
+//
+//        AnimationTimer timer = new AnimationTimer() {
+//            @Override
+//            public void handle(long l) {
+//                render();
+//                update();
+//            }
+//        };
+//        timer.start();
+//
+//        createMap();
+//
+//        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
+//        entities.add(bomberman);
+//    }
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(800);
+        settings.setHeight(600);
+        settings.setTitle("Bomberman");
+        settings.setVersion("0.1");
     }
 
     @Override
-    public void start(Stage stage) {
-        // Tao Canvas
-        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
-        gc = canvas.getGraphicsContext2D();
+    protected void initGame() {
+        player = Entities.builder().at(0,0)
+                .viewFromTexture("player_right.png")
+                .buildAndAttach(getGameWorld());
+    }
 
-        // Tao root container
-        Group root = new Group();
-        root.getChildren().add(canvas);
+    @Override
+    protected void initInput() {
+        Input input = getInput();
 
-        // Tao scene
-        Scene scene = new Scene(root);
-
-        // Them scene vao stage
-        stage.setScene(scene);
-        stage.show();
-
-        AnimationTimer timer = new AnimationTimer() {
+        input.addAction(new UserAction("Move Right") {
             @Override
-            public void handle(long l) {
-                render();
-                update();
+            protected void onAction() {
+                player.translateX(5); // move right 5 pixels
+                getGameState().increment("pixelsMoved", 5);
             }
-        };
-        timer.start();
+        }, KeyCode.RIGHT);
 
-        createMap();
-
-        Entity bomberman = new Bomber(1, 1, Sprite.player_right.getFxImage());
-        entities.add(bomberman);
-    }
-
-    public void createMap() {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
-                Entity object;
-                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
-                    object = new Wall(i, j, Sprite.wall.getFxImage());
-                }
-                else {
-                    object = new Grass(i, j, Sprite.grass.getFxImage());
-                }
-                stillObjects.add(object);
+        input.addAction(new UserAction("Move Left") {
+            @Override
+            protected void onAction() {
+                player.translateX(-5);
             }
-        }
+        }, KeyCode.LEFT);
+
+        input.addAction(new UserAction("Move Up") {
+            @Override
+            protected void onAction() {
+                player.translateY(-5);
+            }
+        }, KeyCode.UP);
+
+        input.addAction(new UserAction("Move Down") {
+            @Override
+            protected void onAction() {
+                player.translateY(5);
+            }
+        }, KeyCode.DOWN);
     }
 
-    public void update() {
-        entities.forEach(Entity::update);
+    @Override
+    protected void initUI() {
+        Text textPixels = new Text();
+        textPixels.setTranslateX(50); // x = 50
+        textPixels.setTranslateY(100); // y = 100
+
+        getGameScene().addUINode(textPixels); // add to the scene graph
     }
 
-    public void render() {
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        stillObjects.forEach(g -> g.render(gc));
-        entities.forEach(g -> g.render(gc));
+    @Override
+    protected void initGameVars(Map<String, Object> vars) {
+        vars.put("pixelsMoved", 0);
     }
+
+//    public void createMap() {
+//        for (int i = 0; i < WIDTH; i++) {
+//            for (int j = 0; j < HEIGHT; j++) {
+//                Entity object;
+//                if (j == 0 || j == HEIGHT - 1 || i == 0 || i == WIDTH - 1) {
+//                    object = new Wall(i, j, Sprite.wall.getFxImage());
+//                }
+//                else {
+//                    object = new Grass(i, j, Sprite.grass.getFxImage());
+//                }
+//                stillObjects.add(object);
+//            }
+//        }
+//    }
+
+//    public void update() {
+//        entities.forEach(Entity::update);
+//    }
+//
+//    public void render() {
+//        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+//        stillObjects.forEach(g -> g.render(gc));
+//        entities.forEach(g -> g.render(gc));
+//    }
 }
